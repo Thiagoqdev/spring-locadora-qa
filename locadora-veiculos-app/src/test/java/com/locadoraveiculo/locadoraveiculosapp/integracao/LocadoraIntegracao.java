7809e1321cf373e2ee4f6fac2fd2d79b36a1ab27
Gustavo Habib <gushabib@gmail.com>
2024-12-04 14:14:34 -0300

    Alterando os testes, para que cada uma tenha sua responsabilidade diferente.

--- locadora-veiculos-app/src/test/java/com/locadoraveiculo/locadoraveiculosapp/integracao/LocadoraIntegracao.java
+++ locadora-veiculos-app/src/test/java/com/locadoraveiculo/locadoraveiculosapp/integracao/LocadoraIntegracao.java
@@ -2,8 +2,7 @@ package com.locadoraveiculo.locadoraveiculosapp.integracao;
 
 import com.locadoraveiculo.locadoraveiculosapp.dto.UserRole;
 import com.locadoraveiculo.locadoraveiculosapp.model.UserEntity;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -11,19 +10,24 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LocadoraIntegracao {
 
     @Autowired
     TestRestTemplate restTemplate;
 
+    private String Token;
+
     @Test
-    @DisplayName("Deve registrar um novo usuário no banco de dados e logar recebendo o token JWT em seguida fazer consulta em veículos com o Token")
+    @Order(1)
+    @DisplayName("Deve registrar um novo usuário no banco de dados e logar recebendo o token JWT")
     void test01(){
         UserEntity userEntity = new UserEntity();
         userEntity.setLogin("test01");
@@ -36,19 +40,30 @@ class LocadoraIntegracao {
 
         ResponseEntity<String> ResponseEntityLogin = restTemplate.postForEntity("/auth/login", userEntity, String.class);
 
-        var token = ResponseEntityLogin.getBody();
-        int colonIndex = token.indexOf(":");
-        String result = token.substring(colonIndex + 1);
-        result = result.replaceAll("[{}\"]", "");
+        assertEquals(200, ResponseEntityLogin.getStatusCodeValue());
+        assertNotNull(ResponseEntityLogin.getBody());
+
+        var result = ResponseEntityLogin.getBody();
+        int doisPontosIndex = result.indexOf(":");
+        this.Token = result.substring(doisPontosIndex + 1).replaceAll("[{}\"]", "");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Deve fazer consulta em veículos com o Token criado no test01")
+    void test02(){
+
+        System.out.println(this.Token);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + result);
+        headers.set("Authorization", "Bearer " + this.Token);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         ResponseEntity<String> ResponseEntityVeiculos = restTemplate.exchange("/api/veiculos", HttpMethod.GET, entity, String.class);
 
         assertEquals(200, ResponseEntityVeiculos.getStatusCodeValue());
+
     }
 
 }
