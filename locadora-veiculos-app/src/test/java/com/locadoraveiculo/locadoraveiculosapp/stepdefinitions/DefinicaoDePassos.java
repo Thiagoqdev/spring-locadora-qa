03989e040012e5f60a10944dd8a6e06a198c45d7
Gustavo Habib <gushabib@gmail.com>
2024-12-04 17:43:32 -0300

    Incluindo o cucumber, e criando um teste com ele.

---
+++ locadora-veiculos-app/src/test/java/com/locadoraveiculo/locadoraveiculosapp/stepdefinitions/DefinicaoDePassos.java
@@ -0,0 +1,48 @@
+package com.locadoraveiculo.locadoraveiculosapp.stepdefinitions;
+
+import com.locadoraveiculo.locadoraveiculosapp.dto.UserRole;
+import com.locadoraveiculo.locadoraveiculosapp.model.UserEntity;
+import com.locadoraveiculo.locadoraveiculosapp.repository.UserRepository;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class DefinicaoDePassos {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Given("o banco de dados deve estar limpo")
+    public void oBancoDeDadosEstaLimpo(){
+        userRepository.deleteAll();
+    }
+
+    @When("cadastrar um novo usuário com nome {string}, com a senha {string} e o tipo de usuario {string}")
+    public void cadastrarUmNovoUsuárioComNomeComASenhaEOTipoDeUsuario(String login, String password, String role) {
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setLogin(login);
+        userEntity.setPassword(password);
+        userEntity.setRole(UserRole.valueOf(role));
+
+        userRepository.save(userEntity);
+    }
+
+    @Then("o usuario com nome {string} deve estar salvo no banco de dados")
+    public void oUsuarioComNomeDeveEstarSalvoNoBancoDeDados(String login) {
+        Optional<UserEntity> useroptional = userRepository.findByLogin(login);
+        assertEquals(login, useroptional.get().getLogin());
+    }
+}
